e1c995861e1f8099a7f0c6cc9065b1f3c26eece9
edupoly <info.edupoly@gmail.com>
2023-06-05 19:28:49 +0530

    sdfsdf

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="border border-2 border-danger m-2 p-2">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
